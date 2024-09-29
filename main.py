b3debb5aafb07b68859a865dfe8480014bd1e2fa
Kabeeraa27 <kabeeer27@gmail.com>
2024-09-30 01:45:20 +0530

    Model Training Added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
 
 
 STAGE_NAME = "DATA INGESTION STAGE"
@@ -23,6 +25,19 @@ if __name__ == "__main__":
         logger.info(f"************************************************")
         logger.info(f">>>>>>>>>> STAGE {STAGE_NAME} STARTED <<<<<<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>> STAGE {STAGE_NAME} COMPLETED <<<<<<<<<<\n\nx=========================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "TRAINING"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"************************************************")
+        logger.info(f">>>>>>>>>> STAGE {STAGE_NAME} STARTED <<<<<<<<<<")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>>>>> STAGE {STAGE_NAME} COMPLETED <<<<<<<<<<\n\nx=========================x")
     except Exception as e:
---
+++ src/cnnClassifier/pipeline/stage_03_model_training.py
@@ -0,0 +1,34 @@
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.model_training import Training
+from cnnClassifier import logger
+
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier import logger
+
+STAGE_NAME = "TRAINING"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+    
+
+    
+if __name__ == "__main__":
+    try:
+        logger.info(f"************************************************")
+        logger.info(f">>>>>>>>>> STAGE {STAGE_NAME} STARTED <<<<<<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>> STAGE {STAGE_NAME} COMPLETED <<<<<<<<<<\n\nx=========================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
